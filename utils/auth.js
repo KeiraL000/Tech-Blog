c6406026b6db11f430987223a01c5543698b02b8
KeiraL000 <keira5466@yahoo.com>
2022-09-02 01:18:43 -0700

    adding code

--- utils/auth.js
+++ utils/auth.js
@@ -6,4 +6,20 @@ const withAuth = (req, res, next) => {
     }
 };
 
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.jwtToken);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
+
 module.exports = withAuth;
